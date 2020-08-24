cd396c630c46792e9c2274460335231a5acf0e0a
Kristina Sachenko <mytino4ka@mail.ru>
2020-08-25 00:17:36 +0700

    Замена некорректных тестов

--- src/main/java/ru/netology/statistic/StatisticService.java
+++ src/main/java/ru/netology/statistic/StatisticService.java
@@ -5,50 +5,73 @@ public class StatisticService {
         int sum = 0;
 
         for (int item : array) {
-            sum +=item;
+            sum += item;
         }
 
         return sum;
     }
 
-    public int findMax(int[] purchases) {
-        int max = purchases [0];
-
-        for (int purchase : purchases) {
-            if (max < purchase){
-
-                max = purchase;
-            }
-        }
-
-        return max;
-    }
-
     public int findMin(int[] purchases) {
-        int min = purchases [0];
-
-        for (int purchase : purchases) {
-            if (min > purchase){
-                min = purchase;
+        int monthNumber = 0;
+        int min = purchases[0];
+        for (int i = 1; i < purchases.length; i++) {
+            int count = purchases[i];
+            if (min <= count) {
+                min = count;
+                monthNumber = i + 1;
             }
         }
+        return monthNumber;
 
-        return min;
     }
 
     public int averageAmount(int[] purchases) {
         int sum = 0;
 
         for (int item : purchases) {
-            sum +=item;
+            sum += item;
         }
-        int amount = sum /12;
+        int amount = sum / 12;
 
         return amount;
 
 
-
     }
 
+    public int findMax(int[] purchases) {
+        int monthNumber = 0;
+        int max = purchases[0];
+        for (int i = 1; i < purchases.length; i++) {
+            int count = purchases[i];
+            if (max <= count) {
+                max = count;
+                monthNumber = i + 1;
+            }
+        }
+        return monthNumber;
+    }
+
+    public int amountforMore(int[] purchases) {
+        int monthCount = 0;
+        int medSum = averageAmount(purchases);
+        for (int arr : purchases) {
+            if (arr > medSum) {
+                monthCount++;
+            }
+        }
+        return monthCount;
+    }
+
+    public int middle(int[] purchases) {
+        int monthCount = 0;
+        int medSum = averageAmount(purchases);
+        for (int arr : purchases) {
+            if (arr < medSum) {
+                monthCount++;
+            }
+        }
+        return monthCount;
+
+    }
 
 }
--- src/test/java/ru/netology/statistic/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticServiceTest.java
@@ -1,8 +1,6 @@
 package ru.netology.statistic;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,18 +8,21 @@ class StatisticServiceTest {
 
 
     @Test
+        //сумма всех продаж
     void shouldCalculate() {
-        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-
         StatisticService service = new StatisticService();
+        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 180;
+
         int actual = service.calculateSum(purchases);
 
-        int expected = 180;
         assertEquals(expected, actual);
 
     }
+
     @Test
-    void averageAmount (){
+        //средняя сумма продаж в месяц
+    void averageAmount() {
         StatisticService service = new StatisticService();
 
         int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
@@ -32,86 +33,56 @@ class StatisticServiceTest {
         assertEquals(expected, actual);
     }
 
-//    @ParameterizedTest
-//    @CsvSource(value =
-//    {"1",
-//    "2",
-//    "3",
-//    "4",
-//    "5",
-//    "6",
-//    "7",
-//    "8",
-//    "9",
-//    "10",
-//    "11",
-//    "12"
-//    }, delimiter = ',')
-//    void findMax() {
-//        StatisticService service = new StatisticService();
-//        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-//        int expected = 20;
-//        int[] numbers = new int[12];
-//        for (int i = 0;
-//             i < purchases.length; i++);
-//
-//        System.out.println(numbers.length);
-//
-//        int actual = service.findMax(purchases);
-//
-//        assertEquals(expected, actual);
-//
-//    }
-//
     @Test
-    void findMin (){
+        //номер месяца в котором был пик продаж
+    void findMax() {
         StatisticService service = new StatisticService();
 
         int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        int expected = 7;
+        int expected = 8;
 
+        int actual = service.findMax(purchases);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+        //месяц в котором был минимум продаж
+    void findMin() {
+        StatisticService service = new StatisticService();
+
+        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8;
 
         int actual = service.findMin(purchases);
 
         assertEquals(expected, actual);
     }
 
-
-
-//    @Test
-//    viod amountforMore () {
-//        StatisticService service = new StatisticService();
-//
-//        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-//        int expected = 7;
-//
-//        int actual = service.amountforMore (purchases);
-//
-//        assertEquals(expected, actual);
-//
-//    }
     @Test
-    void findMax () {
-    StatisticService service = new StatisticService();
+        //продажи выше среднего
+    void amountforMore() {
+        StatisticService service = new StatisticService();
 
-    int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-    int expected = 20;
-    for (int i = 0; i < purchases.length; i++) {
-        System.out.println(i);
-    }
-
-    int actual = service.findMax(purchases);
-
-    assertEquals(expected, actual);
-
-    }
-
-    @Test
-    void month (){
         int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
 
-        for (int i = 0; i < purchases.length; i++){
-            System.out.println(i);
-        }
+        int actual = service.amountforMore(purchases);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+        //количество месяцев ниже среднего
+    void middle() {
+        StatisticService service = new StatisticService();
+        int[] purchases = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.middle(purchases);
+
+        assertEquals(expected, actual);
+
     }
 }
